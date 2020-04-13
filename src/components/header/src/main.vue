77308963e2fc9c025601b2b57c179e97c977829a
it_dog <it_dog_zhang@163.com>
2020-04-13 22:58:19 +0800

    add layout

---
+++ src/components/header/src/main.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="zh-header">xx</div>
+</template>
+
+<script>
+import './main.scss'
+export default {
+  name: 'ZhHeader',
+
+  componentName: 'Header',
+
+  props: {
+    height: {
+      type: String,
+      default: '60px'
+    }
+  }
+}
+</script>
--- src/layout/LayOut.vue
+++ src/layout/LayOut.vue
@@ -1,44 +1,34 @@
 <template>
-  <el-container class="page">
-    <el-header height="60px" class="header">Header</el-header>
-    <el-container class="container">
-      <el-aside width="200px" class="aside">Aside</el-aside>
-        <el-container>
-            <el-main class="main">
-                <router-view/>
-            </el-main>
-            <el-footer>footer</el-footer>
-        </el-container>
-    </el-container>
-  </el-container>
+  <div>
+    <div class="header">
+      <zh-header />
+    </div>
+    <div class="content">
+      <div class="aside">
+        <zh-aside/>
+      </div>
+      <div class="main-content">
+        <div class="main">
+          <div class="page">
+            <router-view />
+          </div>
+          <div class="footer">footer</div>
+        </div>
+
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import './Layout.scss'
+import ZhHeader from '@/components/header'
+import ZhAside from '@/components/aside'
 export default {
-  name: 'LayOut'
+  name: 'LayOut',
+  components: {
+    ZhHeader,
+    ZhAside
+  }
 }
 </script>
-
-<style scoped>
-    .page{
-        margin: 0;
-        padding: 0;
-    }
-    .header{
-        background-color: azure;
-    }
-    .aside{
-        background-color: #42b983;
-        height: 100%;
-        min-height: 600px;
-    }
-    .main{
-        background-color: green;
-        padding: 25px;
-        min-height: 556px;
-    }
-    .footer{
-        height: 44px;
-        background-color: red;
-    }
-</style>
